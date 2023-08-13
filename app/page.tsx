774db802ad1ca90c020d6ccc78b3e72508ad8af9
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-08-13 22:27:29 +0600

    DeskNav options added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="max-w-7xl m-auto">
       <Navbar />
       <h1 className="text-5xl font-bold text-blue-800">Fashion</h1>
     </main>
--- components/Navbar/DeskNav/index.tsx
+++ components/Navbar/DeskNav/index.tsx
@@ -2,18 +2,56 @@ import Image from 'next/image';
 
 export default function DeskNav() {
   return (
-    <nav>
-      <div className="flex justify-center items-center">
-        <div className="mr-1">
-          <Image
-            src="/images/Vector.svg"
-            alt="Fashion"
-            width={30}
-            height={30}
-          />
-        </div>
-        <h1 className="uppercase text-3xl font-extrabold">Fashion</h1>
-      </div>
+    <nav className="">
+      <ul className="flex justify-between">
+        <li className="flex justify-center items-center">
+          <div className="mr-1">
+            <Image
+              src="/images/Vector.svg"
+              alt="Fashion"
+              width={30}
+              height={30}
+            />
+          </div>
+          <h1 className="uppercase text-3xl font-extrabold">Fashion</h1>
+        </li>
+
+        <li className="">
+          <ul className=" flex justify-center items-center">
+            <li>
+              <div>
+                <button className=" text-[22px] m-5">Catalogue</button>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <button className=" text-[22px] m-5">Fashion</button>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <button className=" text-[22px] m-5">Favourite</button>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <button className=" text-[22px] m-5">Lifestyle</button>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <button className=" text-[22px] m-5 bg-gray-950 rounded-md text-white p-2">
+                  Sign Up
+                </button>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </nav>
   );
 }
