fdea38fa754239a7290169ff67e8fd0d096d8396
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-08-13 22:03:56 +0600

    add fashion logo

--- components/Navbar/DeskNav/index.tsx
+++ components/Navbar/DeskNav/index.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
 export default function DeskNav() {
   return (
-    <div>
-      <h1>DeskNav</h1>
-    </div>
+    <nav>
+      <div className="flex justify-center items-center">
+        <div className="mr-1">
+          <Image
+            src="/images/Vector.svg"
+            alt="Fashion"
+            width={30}
+            height={30}
+          />
+        </div>
+        <h1 className="uppercase text-3xl font-extrabold">Fashion</h1>
+      </div>
+    </nav>
   );
 }
