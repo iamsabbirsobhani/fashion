b3897414960bbee7082dc45bc93c145d41cbc4b3
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-08-20 22:30:15 +0600

    modify section two

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className=" m-auto">
+    <main className="m-auto">
       <Navbar />
       <SectionOne />
       <SectionTwo />
