9d3f282a78ec156e04ad6ae8bcb3a0041c75f1a3
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-08-24 23:58:24 +0600

    add section two hdotm logo

--- components/section-two/index.tsx
+++ components/section-two/index.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function SectionTwo() {
   return (
     <div className=" w-full bg-[#EBD96B] p-10 mt-14 flex justify-between">
-      <div>Him</div>
+      <div>
+        <Image
+          src="/images/section-two/hdotm.svg"
+          alt="section-two"
+          width={500}
+          height={500}
+        />
+      </div>
       <div>Obey</div>
       <div>Shopify</div>
       <div>Lacoste</div>
