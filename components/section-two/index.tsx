1bc4837f35d53b512394d808249b1a5750559858
iamsabbirsobhani <sabbirsobhani@gmail.com>
2023-08-25 17:31:32 +0600

    add modified logo to section two

--- components/section-two/index.tsx
+++ components/section-two/index.tsx
@@ -2,19 +2,56 @@ import Image from 'next/image';
 
 export default function SectionTwo() {
   return (
-    <div className=" w-full bg-[#EBD96B] p-10 mt-14 flex justify-between">
+    <div className=" w-full bg-[#EBD96B] p-10 mt-14 flex justify-between items-center">
       <div>
         <Image
-          src="/images/section-two/hdotm.svg"
+          src="/images/section-two/hm-logo.png"
           alt="section-two"
-          width={500}
-          height={500}
+          width={120}
+          height={60}
         />
       </div>
-      <div>Obey</div>
-      <div>Shopify</div>
-      <div>Lacoste</div>
-      <div>Levis</div>
+      <div>
+        <Image
+          src="/images/section-two/obey.png"
+          alt="section-two"
+          width={120}
+          height={60}
+        />
+      </div>
+      <div>
+        <Image
+          src="/images/section-two/shopify.png"
+          alt="section-two"
+          width={120}
+          height={60}
+        />
+      </div>
+      <div>
+        <Image
+          src="/images/section-two/lacoste.png"
+          alt="section-two"
+          width={120}
+          height={60}
+        />
+      </div>
+      <div>
+        <Image
+          src="/images/section-two/levis.png"
+          alt="section-two"
+          width={120}
+          height={60}
+        />
+      </div>
+
+      <div>
+        <Image
+          src="/images/section-two/amazon.png"
+          alt="section-two"
+          width={120}
+          height={60}
+        />
+      </div>
     </div>
   );
 }
